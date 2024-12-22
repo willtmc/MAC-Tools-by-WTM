5a53406720fc9fc14e4b250f61c4bc832c8f2176
willtmc <will@mclemoreauction.com>
2024-12-21 22:31:43 -0600

    Add backups directory to .gitignore

--- backups/2023-12-21_1642/tools/neighbor_letters/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-neighbor_letters_bp = Blueprint('neighbor_letters', __name__, url_prefix='/neighbor_letters')
-
-from . import routes
--- backups/2023-12-21_1642/tools/qr_labels/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-qr_labels_bp = Blueprint(
-    'qr_labels_bp', 
-    __name__, 
-    template_folder='templates'
-)
-
-from . import routes
